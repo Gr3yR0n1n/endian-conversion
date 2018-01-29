116c6e99ed2d202778680de377907c6f47cd2254
invictus-c <35806164+invictus-c@users.noreply.github.com>
2018-01-28 20:48:41 -0600

    Add files via upload

--- endian.c
+++ endian.c
@@ -6,6 +6,7 @@ uint32_t big_endian2_little_endian(uint32_t s)
     uint32_t b2 = (s & 0x00ff0000) >> 16u;
     uint32_t b3 = (s & 0x0000ff00) >> 8u;
     uint32_t b4 = (s & 0x000000ff);
+
     return (b4 << 24u)
             | (b3 << 16u)
             | (b2 << 8u )
